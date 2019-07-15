321b37a4447dc90ef866e2a2442b981e15b448e6
2573722674 <2573722674@qq.com>
2019-07-15 17:18:02 +0800

    2 commit

--- app/src/main/java/com/example/usedmarket/MainActivity.java
+++ app/src/main/java/com/example/usedmarket/MainActivity.java
@@ -1,18 +1,39 @@
 package com.example.usedmarket;
 
 import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.View;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int TIME=3000;
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent=new Intent(MainActivity.this, Login.class);
-        startActivity(intent);
-        finish();
+        getWindow().setFormat(PixelFormat.RGBA_8888);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DITHER);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        },TIME);
+
+
+
     }
 
 
